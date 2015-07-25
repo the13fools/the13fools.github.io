5a9fb996afa4d97f26dac14a950da889d9809caa
Josh Vekhter <josh@formlabs.com>
2015-07-25 13:00:43 -0400

    Visualize primary harmonic

--- lets-get-lost/js/rope.js
+++ lets-get-lost/js/rope.js
@@ -1,11 +1,11 @@
 
 var nodes = 10;
 
-var MASS = .1;
-var restDistance;
-var springConstant = 1000;
+var MASS = .01;
+var springRestDistance;
+var springConstant = 10000;
 
-var DAMPING = 0;
+var DAMPING = 0.00001;
 
 var TIMESTEP = 10 / 10000;
 var TIMESTEP_SQ = TIMESTEP * TIMESTEP;
@@ -35,15 +35,22 @@ Particle.prototype.getAcceleration = function() {
 		return this.a;
 }
 
+function Constraint(p1, p2, restLength, allowedStretch) {
+	this.p1 = p1;
+	this.p2 = p2;
+	this.restLength = restLength;
+	this.allowedStretch = allowedStretch;
+}
+
 function Rope(nodes) {
 	nodes = nodes || 10;
 	this.nodes = nodes;
 	var tension = .1;
-	restDistance = 1 / ((nodes - 1) * 10);
-
+	springRestDistance = 1 / ((nodes - 1) * 1.1);
 
 	var particles = [];
 	var springs = [];
+	var constraints = [];
 
 	// nodes
 	for (i = 0; i < nodes; i++) {
@@ -52,18 +59,33 @@ function Rope(nodes) {
 			);
 	}
 
+	// TODO: make this it's own object
 	// springs
 	for (i = 0; i < nodes - 1; i++) {
 		springs.push([
 				particles[i],
 				particles[i + 1],
-				restDistance,
+				springRestDistance,
 				springConstant
 			]);
 	}
 
+
+	// constraints (to model connections as inf strength springs beyond certain point)
+	stretchPercentage = .01;
+	restDistance = 1 / (nodes - 1);
+	for (i = 0; i < nodes - 1; i++) {
+		constraints.push(new Constraint(
+				particles[i],
+				particles[i + 1],
+				restDistance,
+				stretchPercentage));
+	}
+
+
 	this.particles = particles;
 	this.springs = springs;
+	this.constraints = constraints;
 
 }
 
@@ -78,8 +100,6 @@ Rope.prototype.removePreviousSpringForces = function() {
 
 		// be careful with the signs here
 		diff.multiplyScalar((this.springs[i][2] - len) * 
-			(this.springs[i][2] - len) * 
-			(this.springs[i][2] - len) * 
 			springConstant);
 
 		this.particles[this.springs[i][0].idx].forces.sub(diff);
@@ -94,13 +114,29 @@ Rope.prototype.addSpringForces = function() {
 		var len = diff.length();
 
 		// be careful with the signs here
-		diff.multiplyScalar((this.springs[i][2] - len) * 
-			(this.springs[i][2] - len) * 
-			(this.springs[i][2] - len) * 
+		diff.multiplyScalar((this.springs[i][2] - len) *
 			springConstant);
 
 		this.particles[this.springs[i][0].idx].forces.add(diff);
 		this.particles[this.springs[i][1].idx].forces.sub(diff);
+	}
+
+};
+
+Rope.prototype.enforceConstraints = function() {
+	for (i = 0; i < this.constraints.length; i++) {
+		var con = this.constraints[i];
+		diff.subVectors(con.p1.position, 
+						con.p2.position);
+		var len = diff.length();
+		var targetDist = con.restLength * (1 + con.allowedStretch);
+
+		if (len > targetDist || len < con.restLength) {
+			var correction = diff.multiplyScalar(1 - targetDist / len);
+			var correctionHalf = correction.multiplyScalar(0.5);
+			con.p1.position.sub(correctionHalf);
+			con.p2.position.add(correctionHalf);
+		}
 	}
 
 };
@@ -126,10 +162,11 @@ Particle.prototype.stepForward = function(timesq) {
 
 // This is down here because function calls need to come after definitions.  
 // Should really exist across a few files.
-var rope = new Rope(30);
+var rope = new Rope(100);
 var driveTime = 0;
-rope.addSpringForces(1);
-rope.particles[1].position.setX(rope.particles[1].position.x + .001);
+//rope.addSpringForces();
+rope.particles[ ~~(rope.nodes / 2)].position
+	.setY(rope.particles[~~(rope.nodes / 2)].position.y + .1);
 // rope.particles[1].position.setX(1 / (rope.nodes - 1));
 
 var colors = ["#a50026",
@@ -155,14 +192,18 @@ function simulate(time) {
 	// rope.particles[1].position.setX(1 / rope.nodes);
 
 	// Step through the simulation several times per animation
-	for (step = 0; step < 10; step++) {
-		rope.removePreviousSpringForces();
-		rope.addSpringForces();
+	for (step = 0; step < 1; step++) {
+//		rope.removePreviousSpringForces();
+//		rope.addSpringForces();
 
 		for (i = 1; i < rope.nodes - 1; i++) {
 	//		console.log("node #"+ i + " : Force " + rope.particles[i].forces.x + "   Pos " + rope.particles[i].position.x);
 			rope.particles[i].stepForward(TIMESTEP_SQ);
 		}
+
+		  rope.enforceConstraints();
+		  rope.particles[0].position.set(0, 0, 0);
+		  rope.particles[rope.nodes - 1].position.set(1, 0, 0);
 	}
 
 	var canvas = document.getElementById("rope-canvas");
